4e8b3cf6e816f0ca276f1feffb2a4f90b9e18be8
Mohammed237 <mohammedmostafa2372002@gmail.com>
2026-01-02 22:15:05 +0200

    Make custom user from abstract user of django

--- services/user service/config/settings.py
+++ services/user service/config/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Point to the app_name.ModelName
+AUTH_USER_MODEL = "users.CustomUser"
