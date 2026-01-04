1dcd0017e0e0d6b46a9efe1e830b619bffbce205
Mohammed237 <mohammedmostafa2372002@gmail.com>
2026-01-04 20:30:22 +0200

    Use redis for caching products

--- services/product service/config/settings.py
+++ services/product service/config/settings.py
@@ -130,3 +130,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Caching Configuration
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        # 'redis' is the hostname from docker-compose
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
--- services/product service/products/views.py
+++ services/product service/products/views.py
@@ -1,12 +1,16 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from django.core.cache import cache
 
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 
 
 # Create your views here.
-class CategoryViewSet(ReadOnlyModelViewSet):  # ReadOnlyModelViewSet provides list and retrieve, blocks post and delete
+class CategoryViewSet(
+    ReadOnlyModelViewSet
+):  # ReadOnlyModelViewSet provides list and retrieve, blocks post and delete
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [AllowAny]
@@ -16,3 +20,23 @@ class ProductViewSet(ReadOnlyModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [AllowAny]
+
+    def list(self, request, *args, **kwargs):
+        cache_key = "product_list"
+
+        # try to fetch from redis
+        cached_data = cache.get(cache_key)
+        if cached_data:
+            print("🚀 Serving from Redis Cache", flush=True)
+            return Response(cached_data)
+
+        # if missed, fetch from database
+        print("🐢 Serving from Database", flush=True)
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        data = serializer.data
+
+        # save to redis for next time
+        cache.set(cache_key, data, timeout=60 * 15)
+
+        return Response(data)
